60719f6fb9ed07012c574a5ba8db61d762e7764f
阿笨 <rushairer@163.com>
2017-04-05 17:01:44 +0800

    [Modify]update setup.sh for vimrc

--- setup.sh
+++ setup.sh
@@ -3,3 +3,6 @@ sudo apt-get install -y zsh vim
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 cp ./resources/zshrc ~/.zshrc
+cp -r resources/vim ~/.vim
+cp ~/.vim/.vimrc ~/.vimrc
+vim +PluginInstall +qall
