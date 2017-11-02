1845ca46b7826f47d81362da122bf0636e9bccfc
Abenx <rushairer@163.com>
2017-11-02 12:44:40 +0800

    [Modify]修改安装流程

--- setup_centos.sh
+++ setup_centos.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo yum install -y zsh vim
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+yum install -y zsh vim
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sed 's/env zsh/#env zsh/g')"
 
 cp ./resources/zshrc ~/.zshrc
 cp -r resources/vim ~/.vim
--- setup_debian.sh
+++ setup_debian.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo apt-get install -y zsh vim
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+apt-get install -y zsh vim
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sed 's/env zsh/#env zsh/g')"
 
 cp ./resources/zshrc ~/.zshrc
 cp -r resources/vim ~/.vim
